f84aafa0ca20545a9633d002cd7a9d7aabc73d3f
Rdev589 <nix3280@gmail.com>
2015-07-24 14:38:18 -0600

    initial mods

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve - This function creates the inverse of a
+## matrix and caches the result.
+##
+## Assumptions:
+##               1) the matrix is square
+##               2) the matrix is invertible
+##
+## The solve function is recommended in the assignment
+## one can also use the ginv function in the MASS package
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m  
 }
